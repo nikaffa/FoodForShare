899518eaa6460d9f4ef34f3c72fc583586aa60de
Anam Shaikh <anam.shk@outlook.com>
2022-01-18 17:11:01 -0500

    using react styled components

--- users/src/App.js
+++ users/src/App.js
@@ -1,33 +1,36 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Donation from "./components/Pages/Donation";
 import Donations from "./components/Pages/Donations";
 import Reservation from "./components/Pages/Reservation";
 import Reservations from "./components/Pages/Reservations";
 import Search from "./components/Pages/Search";
-import Main from "./components/Pages/Main";
-import Layout from "./components/Layout";
 import './App.scss';
-
-import { Navbar, Container, Nav, Carousel } from "react-bootstrap";
+import Main from "./components/Pages/Main";
 
 function App() {
   return (
-    <React.Fragment>
-      <Layout>
-        <Router>
-            {/* <Route exact path='/' component={Main}/> */}
-            <Route path='/donation' component={Donation}/>
-            <Route path='/donations' component={Donations}/>
-            <Route path='/reservation' component={Reservation}/>
-            <Route path='/reservations' component={Reservations}/>
-            <Route path='/Search' component={Search}/>
-            <Route component={Error}/>
-        </Router>
-        <header>HELLO</header>
-      </Layout>
-    </React.Fragment>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Main}/>
+          
+          {/* <Route path='/donation/new' component={Donation}/>
+          <Route path='/donations' component={Donations}/>
+          <Route path='/reservation/new' component={Reservation}/>
+          <Route path='/reservations' component={Reservations}/>
+          <Route path='/Search' component={Search}/>
+          <Route component={Error}/> */}
+        </Switch>
+      </Router>
+    </div>
   );
+}
+
+function Home () {
+  return (
+    <h2>I am here</h2> 
+  )
 }
 
 export default App;
---
+++ users/src/components/responsive/index.js
@@ -0,0 +1,3 @@
+export const deviceSize = {
+  mobile: 720,
+};
