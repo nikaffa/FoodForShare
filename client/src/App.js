95a12711b2879923418d1a4327285cbb51c85806
Anam Shaikh <anam.shk@outlook.com>
2022-01-19 16:47:51 -0500

    added donation/new route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home"
+import Donation from "./pages/Donation"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}>
+        </Route>
+        <Route path="/donation/new" element={<Donation/>}>
         </Route>
       </Routes>
     </Router>
