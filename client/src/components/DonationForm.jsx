85bf55ab84fef5325a44caf9956c8472c58f89b5
Anam Shaikh <anam.shk@outlook.com>
2022-01-21 17:33:13 -0500

    created state to input fields of donation form

--- client/src/components/DonationForm.jsx
+++ client/src/components/DonationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { Component } from "react";
 import { Marginer } from "./Marginer";
 import { LogoTitle } from "./BrandLogo";
 import {
@@ -11,31 +11,81 @@ import { Link } from "react-router-dom";
 import DropDown from "./FreshnessDropDown";
 import QuantityDropDown from "./QuantityDropDown";
 
+class Form extends Component {
+constructor() {
+   super();
+   this.state = {
+   };
+   this.onInputchange = this.onInputchange.bind(this);
+   this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onInputchange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  onSubmitForm() {
+    console.log(this.state)
+ }
+}
+export default Form;
+
 export function DonationForm(props) {
+  const { items } = this.state;
   return (
     <BoxContainer>
       <LogoTitle>Create a new Donation</LogoTitle>
       <Marginer direction="vertical" margin="3em" />
       <FormContainer>
-        <Input placeholder="Title" />
+        <Input 
+          name="title"
+          type="text"
+          placeholder="Title" 
+          value={this.state.title}
+          onChange={this.onInputchange}
+        />
         <Marginer direction="vertical" margin="1em" />
-        <Input placeholder="FoodType" />
+        <Input name="foodtype"
+          type="text"
+          placeholder="Food Type" 
+          value={this.state.foodtype}
+          onChange={this.onInputchange} 
+        />
         <Marginer direction="vertical" margin="1em" />
-        {/* <Input placeholder="Freshness"> */}
-          <DropDown/>
-        {/* </Input> */}
-        
+          <DropDown name="freshness"
+            type="text"
+            value={this.state.freshness}
+            onChange={this.onInputchange}
+          />
         <Marginer direction="vertical" margin="1em" />
-        <Input type="description" placeholder="Description" />
+        <Input type="description" name="description"
+          type="text"
+          placeholder="Description" 
+          value={this.state.discription}
+          onChange={this.onInputchange} 
+        />
         <Marginer direction="vertical" margin="1em" />
         <QuantityDropDown>
-          <Input/>
+          <Input name="quantity"
+            type="number"
+            value={this.state.quantity}
+            onChange={this.onInputchange}
+          />
         </QuantityDropDown>
         <Marginer direction="vertical" margin="1em" />
-        <Input placeholder="Address" />
+        <Input name="address"
+          type="text" 
+          placeholder="Address" 
+          value={this.state.address}
+          onChange={this.onInputchange}
+        />
       </FormContainer>
       <Marginer direction="vertical" margin="1em" />
-
+      <Link to="/donations">
+        <SubmitButton size={'25px'} onSubmit={this.onSubmitForm}>Save Donation</SubmitButton>
+      </Link>
       <Marginer direction="vertical" margin={5} />
     </BoxContainer>
   );
--- client/src/pages/Donation.js
+++ client/src/pages/Donation.js
@@ -1,46 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { DonationForm } from "../components/DonationForm";
 import Navbar from "../components/Navbar";
 import { BackgroundContainer, InnerPageContainer, PageContainer } from "../components/PageContainer";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 import { Marginer } from "../components/Marginer";
 import { SubmitButton } from "../components/Common";
 import { useState } from "react";
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: `http://localhost:8080/donations/1`
-  })
-  
-  // const {state, setState} = useState({donations:[]})
-  
-  // const createDonation = async() => {
-  //   let res = await api.post('/', {name: donation.name, foodType: donation.foodType})
-  //   console.log(res)
-  // }
-  
+const url = `http://localhost:8080/donations/1`;
 
+const { state, setState } = useState({ donations: [] });
+//const { formData, setFormData } =useState({});
 
+const createDonation = (data) => {
+  console.log(state);
+  axios.post(url, data)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
+
+};
 
 export default function Donation(props) {
-  const { action } = useParams();
 
   return (
     <PageContainer>
-      <Navbar/>
-      <Marginer direction="vertical" margin="1em"/>
+      <Navbar />
+      <Marginer direction="vertical" margin="1em" />
       <Link to="/donations">
         <Button size="0.25px">Go Back</Button>
       </Link>
       <InnerPageContainer>
         <BackgroundContainer>
-        <DonationForm>
-        <Link to="/donations">
-          <SubmitButton size={'25px'} onClick={api}>Save Donation</SubmitButton>
-        </Link>
-      </DonationForm>
+          <DonationForm/>
         </BackgroundContainer>
       </InnerPageContainer>
     </PageContainer>
