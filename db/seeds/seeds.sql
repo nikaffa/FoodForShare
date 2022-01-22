6adbe0fb81b16a836e8425f0d6b889eafa5280a2
Veronika Fatikhova <fidelio@list.ru>
2022-01-22 09:19:45 -0800

    seed donation_Items

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -11,13 +11,16 @@ insert into donations (user_id, donation_date, Status) values(2, '4-1-2022', 'Pi
 
 
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(1, 'Pizza', 'Cooked', 'A few slices of pizza', '/images', '04-04-2022', 5, 3);
+                     values(1, 'Pizza', 'Cooked', 'A few slices of pizza', '/images', '2 hours', 5, 3);
                      
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(1, 'Pasta', 'Cooked', '10lbs of Pasta', '/images', '04-04-2022', 10, 8);
+                     values(1, 'Pasta', 'Cooked', '10lbs of Pasta', '/images', '8 hours', 10, 8);
 
+insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
+                     values(2, 'Canned beans', 'Canned', '10 cans of black beans', '/images', '12 months', 10, 10);
 
-
+insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
+                     values(2, 'Gala apples', 'Raw', '5lbs of local apples', '/images', '8 days', 5, 2);
 
 
 insert into reservations (user_id, reservation_date, status) values (2, '2-1-2022', 'Waiting');
