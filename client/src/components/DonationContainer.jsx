5904599253b61c45d70b0a6e8f8e07b7ad0c4f12
Veronika Fatikhova <fidelio@list.ru>
2022-01-22 21:06:09 -0800

    change DonationContainer

--- client/src/components/DonationContainer.jsx
+++ client/src/components/DonationContainer.jsx
@@ -1,7 +1,4 @@
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { SubmitButton } from './Common'
-import { Marginer } from './Marginer'
 import { InnerPageContainer } from './PageContainer'
 
 const DonationCard = styled.div`
