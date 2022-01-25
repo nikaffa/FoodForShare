98dcad5c01feb8cf6732222805c02ff48da77a29
Kashif <km092009@gmail.com>
2022-01-24 22:32:30 -0500

    Reservations listing completed

--- routes/reservations.js
+++ routes/reservations.js
@@ -16,51 +16,55 @@ module.exports = (db) => {
       setTimeout(() => response.status(500).json({}), 1000);
       return;
     }
-    const cart = request.body;
+    const {user, cart } = request.body;
+    console.log(request.body)
     let val = "";
-    let user_id = "2";
     for (const key in cart) {
       const { id, qty } = cart[key];
       console.log(id, qty)
       val += `((select id from inserted_id), ${cart[key].id}, ${cart[key].qty}), `;
     }
     val = val.substring(0, val.length - 2);
-    const queryString = `WITH inserted_id AS (INSERT INTO reservations (user_id, reservation_date, status) values (${user_id}, Now(), 'Waiting') RETURNING id)
+    const queryString = `WITH inserted_id AS (INSERT INTO reservations (user_id, reservation_date, status) values (${user}, Now(), 'Waiting') RETURNING id)
                          INSERT INTO reservation_items (reservation_id, donation_item_id, quantity)
                          VALUES ${val} RETURNING (select id from inserted_id)`;
     db.query(queryString)
       .then(() => {
         setTimeout(() => {
-          response.status(222).json('Record stored in DB.');
+          response.status(222).json('Donation stored in DB.');
         }, 1000);
       })
       .catch(error => console.log(error));
   });
 
   //(shows user their own reservations)
-  router.get("/", (request, response) => {
-    db.query(`SELECT * FROM reservations`).then(({ rows: donations }) => {
+  router.get("/user/:id", (request, response) => {
+    db.query(`SELECT reservation_date, name, food_type, description, image, freshness, status, reservation_id, reservation_items.quantity FROM reservations
+              INNER JOIN reservation_items ON reservations.id=reservation_id
+              INNER JOIN donation_Items ON donation_Items.id=donation_item_id
+              where user_id=$1::integer`,[request.params.id]).then(({ rows: reservations }) => {
       response.json(
-        donations.reduce(
-          (previous, current) => ({ ...previous, [current.id]: current }),
-          {}
-        )
+        reservations.reduce((groups, item) => ({
+          ...groups,
+          [item.reservation_id]: [...(groups[item.reservation_id] || []), item]
+        }), {})
+
+        
       );
     });
   });
 
   //(shows user their own reservations)
   router.get("/:id", (request, response) => {
-    db.query(`SELECT *, reservation_id FROM reservations INNER JOIN reservation_items ON reservations.id=reservation_id where reservations.id=$1::integer`, [Number(request.params.id)]).then(({ rows: reservations }) => {
+    db.query(`SELECT reservations.*, reservation_id, donation_item_id, quantity FROM reservations right JOIN reservation_items ON reservations.id=reservation_id where reservations.id=$1::integer`, [Number(request.params.id)]).then(({ rows: reservations }) => {
       response.json(
-        reservations.reduce(
+        reservations.map(
           (previous, current) => ({ ...previous, [current.id]: current }),
           {}
         )
       );
     });
   });
-
 
   return router;
 };
