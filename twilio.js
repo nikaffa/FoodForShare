d12a755330d716d1fbb3e4f1af3b288583ee32e4
Veronika Fatikhova <fidelio@list.ru>
2022-02-13 22:28:44 -0800

    update twilio

--- twilio.js
+++ twilio.js
@@ -6,13 +6,12 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-console.log(accountSid)
+console.log(accountSid);
 client.messages
   .create({
-     body: 'Your order is ready, please pick it up while it\'s hot! Your total is $25',
-     from: '+18022558617', //valid Twilio number
-     //to: '+14379896701'
-     to: '+16478741655'
-   })
+    body: 'Your order is ready, please pick it up while it\'s hot!',
+    from: '+1', //valid Twilio number
+    to: '+1'
+  })
   .then(message => console.log(message.sid));
 
