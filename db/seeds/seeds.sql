00ca663b02394760a608d0e2764a024d34ca9e8e
Veronika Fatikhova <fidelio@list.ru>
2022-02-13 23:02:01 -0800

    update db seeds, delete geocoder & mapbox scripts from ejs & .env

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -10,29 +10,29 @@ insert into users (id, name, email, password, address, location, phone )
             values(5, 'Georgia Food Bank', 'xyz@foodbank.com', 'pwd', '701 W Georgia St, Vancouver, BC V7Y 1G5, Canada', '(49.282249, -123.119517)', '6476474677');
 
 
-insert into donations (user_id, donation_date, Status) values(1, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(1, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(2, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(2, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(2, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(2, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(3, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(3, '1-27-2022', 'Pick-Up');
-insert into donations (user_id, donation_date, Status) values(3, '1-27-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(1, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(1, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(2, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(2, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(2, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(2, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(3, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(3, '2-13-2022', 'Pick-Up');
+insert into donations (user_id, donation_date, Status) values(3, '2-13-2022', 'Pick-Up');
 
 
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(1, 'Pizza', 'Cooked', 'A few slices of pizza', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg', '24', 5, 3);
+                     values(1, 'Pizza', 'Cooked', 'A few slices of pizza', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg', '72', 5, 3);
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(1, 'Pasta', 'Cooked', '10lbs of Pasta', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/The_Only_Original_Alfredo_Sauce_with_Butter_and_Parmesano-Reggiano_Cheese.png', '24', 10, 8);
+                     values(1, 'Pasta', 'Cooked', '10lbs of Pasta', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/The_Only_Original_Alfredo_Sauce_with_Butter_and_Parmesano-Reggiano_Cheese.png', '72', 10, 8);
 
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(2, 'Pizza', 'Cooked', 'A few slices of pizza', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg', '24', 5, 3);
+                     values(2, 'Pizza', 'Cooked', 'A few slices of pizza', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg', '72', 5, 3);
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(3, 'Pizza', 'Cooked', 'A few slices of pizza', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg', '24', 5, 3);
+                     values(3, 'Pizza', 'Cooked', 'A few slices of pizza', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg', '72', 5, 3);
 
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
-                     values(4, 'Pasta', 'Cooked', '10lbs of Pasta', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/The_Only_Original_Alfredo_Sauce_with_Butter_and_Parmesano-Reggiano_Cheese.png', '24', 10, 8);
+                     values(4, 'Pasta', 'Cooked', '10lbs of Pasta', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/The_Only_Original_Alfredo_Sauce_with_Butter_and_Parmesano-Reggiano_Cheese.png', '72', 10, 8);
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
                      values(4, 'Baked beans', 'Canned', '10 cans of beans in tomato sause', 'https://media.zenfs.com/en/insider_articles_922/d08341acf94c0784230abbb57a87fd01', '72', 10, 10);
 insert into donation_Items (donation_id, name, food_type, description, image, freshness, quantity, leftover) 
@@ -60,8 +60,8 @@ insert into donation_Items (donation_id, name, food_type, description, image, fr
                      values(9, 'Gala apples', 'Raw', '5lbs of local apples', 'https://images.heb.com/is/image/HEBGrocery/000320644', '72', 5, 2);
 
 
-insert into reservations (user_id, reservation_date, status) values (2, '1-26-2022', 'Waiting');
-insert into reservations (user_id, reservation_date, status) values (2, '1-26-2022', 'Waiting');
+insert into reservations (user_id, reservation_date, status) values (2, '2-13-2022', 'Waiting');
+insert into reservations (user_id, reservation_date, status) values (2, '2-13-2022', 'Waiting');
 
 insert into reservation_items (reservation_id, donation_item_id , quantity) values (1, 1, 2);
 insert into reservation_items (reservation_id, donation_item_id , quantity) values (1, 2, 2);
